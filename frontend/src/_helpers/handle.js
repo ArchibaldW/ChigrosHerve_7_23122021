5a98345ae89788a390ad3106a629d64541e4612f
ArchibaldW <herve@chigros.fr>
2022-01-01 16:32:21 +0100

    Ajout de condition au traitement de réponse

--- frontend/src/_helpers/handle.js
+++ frontend/src/_helpers/handle.js
@@ -1,10 +1,17 @@
+import { authenticationService } from '@/_services';
+
 export const handle = {
     response(res) {
         if (res.ok) {
             return res.json();
         } else {
             return res.json().then(function(data){
-                throw new Error("Une erreur est arrivée : " + res.status + " - " + data.message); 
+                if (res.status == 401 || res.status == 403){
+                    authenticationService.logout();
+                    throw new Error("Une erreur est arrivée : " + res.status + " - action non autorisée"); 
+                } else {
+                    throw new Error("Une erreur est arrivée : " + res.status + " - " + data.message); 
+                }
             })
         }
     },
